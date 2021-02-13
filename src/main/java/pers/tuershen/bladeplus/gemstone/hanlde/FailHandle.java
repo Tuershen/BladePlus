4c7e2a7846f0f3823a141c7f18b517b5a67c3526
Tuershen <1208465031@qq.com>
2021-02-14 00:14:56 +0800

    强化失败处理结果

--- src/main/java/pers/tuershen/bladeplus/gemstone/hanlde/FailHandle.java
+++ src/main/java/pers/tuershen/bladeplus/gemstone/hanlde/FailHandle.java
@@ -1,7 +1,5 @@
 package pers.tuershen.bladeplus.gemstone.hanlde;
 
-import com.tuershen.nbtlibrary.api.NBTTagCompoundApi;
-import com.tuershen.nbtlibrary.minecraft.nbt.TagCompound;
 import com.tuershen.nbtlibrary.minecraft.nbt.TagInt;
 import org.bukkit.entity.Player;
 import pers.tuershen.bladeplus.api.IYamlSetting;
@@ -20,17 +18,12 @@ public class FailHandle extends AbstractHandle {
 
     @Override
     public void handleEvent() {
-        NBTTagCompoundApi nbtTagCompound = plusHandle.getEntityNBTTagCompoundApi().getNBTTagCompound();
-        TagCompound blade = nbtTagCompound.get("Blade");
-        TagCompound tag = blade.getCompound("tag");
         int repairCounter = 0;
         if (tag.hasKey("RepairCounter")) {
             repairCounter = tag.getInt("RepairCounter").getInt() - plusHandle.getBladePlusMaterial().getFail();
         }
         tag.set("RepairCounter", new TagInt(Math.max(repairCounter, 0)));
-        blade.set("tag", tag);
-        nbtTagCompound.set("Blade", blade);
-        plusHandle.getEntityNBTTagCompoundApi().saveNBTTag(nbtTagCompound);
+        this.saveBlade();
     }
 
     @Override
